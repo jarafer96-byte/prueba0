6a9e69220767cb0470e946e0a476e01ee5c72e93
Fernando Jara <jarafer96@gmail.com>
2026-04-06 16:54:15 -0300

    Create fonts.js

---
+++ static/js/fonts.js
@@ -0,0 +1,4 @@
+var link = document.createElement('link');
+link.href = 'https://fonts.googleapis.com/css2?family=Roboto&family=Playfair+Display&family=Raleway&display=swap';
+link.rel = 'stylesheet';
+document.head.appendChild(link);
