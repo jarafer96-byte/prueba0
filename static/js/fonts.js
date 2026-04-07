30b6df31b95d1ccd186bab0cb68e0d7c23a6e4e6
Fernando Jara <jarafer96@gmail.com>
2026-04-07 19:24:29 -0300

    Delete static/js/fonts.js

--- static/js/fonts.js
+++
@@ -1,4 +0,0 @@
-var link = document.createElement('link');
-link.href = 'https://fonts.googleapis.com/css2?family=Roboto&family=Playfair+Display&family=Raleway&display=swap';
-link.rel = 'stylesheet';
-document.head.appendChild(link);
